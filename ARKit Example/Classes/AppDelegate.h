a2d4b8694fbf4ba1507738a325c27aa213c03388
Muhammad Hijazi <irekasoft@gmail.com>
2015-05-17 12:44:06 +0800

    test pushing forking

---
+++ ARKit Example/Classes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARKit Example
+//
+//  Created by Carlos on 21/10/13.
+//
+//
+// hello
+// test
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
